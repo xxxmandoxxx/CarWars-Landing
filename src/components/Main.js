34a1e93da3e0643d453a4a9ac7a3c5f598c83066
xxxmandoxxx <xxxmandoxxx@Philipps-MacBook-Air.local>
2018-04-20 11:00:35 +1000

    added background sound functionality

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import Signup from './Signup';
-import Title from './TitleText'
+import Title from './TitleText';
+import Sound from 'react-sound';
+import BackgroundSound from './sound/background_music.wav';
 
 
 class Main extends React.Component {
+    state = {
+        playMusic: "PLAYING",
+        musicVolume: 50
+    }
+
+    handleSoundButton = (status) => {
+        this.setState({playMusic: status})
+    }
+
+
     render() {
+        let soundButton = null
+
+        if (this.state.playMusic === "PLAYING") {
+            soundButton = <div className="soundButton" onClick={() => this.handleSoundButton("PAUSE")}><i className="fa fa-volume-up" /></div>
+        } else {
+            soundButton = <div className="soundButton soundOff" onClick={() => this.handleSoundButton("PLAYING")}><i className="fa fa-volume-off" /></div>
+        }
+        
+
         let mainView = null;
         
             mainView = [
@@ -13,6 +34,8 @@ class Main extends React.Component {
                 
         return (
             <div>
+                {soundButton}
+                <Sound url={BackgroundSound} playStatus={this.state.playMusic} volume={this.state.musicVolume} loop={true} autoLoad={true} />
                 {mainView}
             </div>
         )
